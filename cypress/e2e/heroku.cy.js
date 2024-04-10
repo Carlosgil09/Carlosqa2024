a3465777094592ad5155e77236c106abd16133f4
carlos <carlosgildelgado43@gmail.com>
2024-04-10 20:22:27 +0200

    Caso invalido

--- cypress/e2e/heroku.cy.js
+++ cypress/e2e/heroku.cy.js
@@ -6,5 +6,14 @@ describe('template spec', () => {
     cy.get('#password')
 
   })
+  
+  it('test de login Con el usuario invalido, () => { 
+    cy.visit('https://the-internet.herokuapp.com/')
+    cy.get(':nth-child(21) > a').click()
+    cy.get('#username').type('pepitolospalotes')   
+    cy.get('#password').type('SuperSecretPassword!')
+    cy.get('.fa').click
+    cy.get('#flash').contains(' Your username is invalid!')
+  }) 
 })
 
